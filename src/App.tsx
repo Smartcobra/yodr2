e84874af24e610ddf3ab1ea82b5668c17c3849b2
saradapc <sarada@yobytech.in>
2024-07-25 12:58:33 +0530

    Insert the WebSite Template Design Page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import LoginComponent from "./auth/LoginComponent";
 import DoctorDS from "./dashboard/DoctorDS";
 import Dashboard from "./dashboard/Dashboard";
 import DrawerScreen from "./commoms/DrawerScreen";
+import Main from "./Main";
 
 
 
@@ -12,7 +13,8 @@ function App() {
   // return <LoginComponent />;
   //return <DrawerScreen/>
   //return <DoctorDS />
-  return <Dashboard />
+  //return <Dashboard />
+  return <Main/>
 }
 
 export default App;
